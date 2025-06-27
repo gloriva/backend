004f36a889bc8f7b9bb81cf5f3ad9b5abaf8a5ec
heondong9265 <heondong0723@gmail.com>
2025-06-27 23:12:28 +0900

    fix: spring-cloud-openfeign RC 버전 이슈 해결

--- bible-service/src/main/java/org/church/bible/Main.java
+++ bible-service/src/main/java/org/church/bible/Main.java
@@ -1,4 +1,4 @@
-package org.church;
+package org.church.bible;
 
 public class Main {
     public static void main(String[] args) {
--- shorturl-service/src/main/java/org/church.shorturl/Main.java
+++ shorturl-service/src/main/java/org/church.shorturl/Main.java
@@ -1,4 +1,4 @@
-package org.church;
+package org.church.shorturl;
 
 public class Main {
     public static void main(String[] args) {
