7819873b8736be7ceb153467e98363edf591462d
heondong9265 <heondong0723@gmail.com>
2025-06-28 16:10:02 +0900

    chore: common/dependencies 공통 모듈 분리

---
+++ services/bible-service/src/main/java/org/church/bible/Main.java
@@ -0,0 +1,6 @@
+package org.church.bible;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ services/shorturl-service/src/main/java/org/church.shorturl/Main.java
@@ -0,0 +1,6 @@
+package org.church.shorturl;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
